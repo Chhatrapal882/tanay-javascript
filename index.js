f783e2f0a66d9091198ef478709497c02fda1a72
chhatrapal882 <prince100tanwar@gmail.com>
2021-09-25 01:47:50 +0530

    docs:adding listne on btn

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('external js')
+alert('this is working')
+var a = prompt('what is your name')
+console.log('aaaaaaaaaaaaa',a)
+
+document.querySelector('#btn').addEventListener('click',()=>{
+    console.log('clicked')
+})
+
