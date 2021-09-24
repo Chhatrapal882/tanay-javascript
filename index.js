03dbf804659ac2290d1a48d6ffda267cfb2ed342
chhatrapal882 <prince100tanwar@gmail.com>
2021-09-25 02:29:36 +0530

    added input and output part

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-console.log('external js')
-alert('this is working')
-var a = prompt('what is your name')
-console.log('aaaaaaaaaaaaa',a)
+// console.log('external js')
+// alert('this is working')
+// var a = prompt('what is your name')
+// console.log('aaaaaaaaaaaaa',a)
 
-document.querySelector('#btn').addEventListener('click',()=>{
-    console.log('clicked')
+
+
+document.querySelector('#translate').addEventListener('click',()=>{
+    var input = document.getElementById('input').value
+    var output = document.getElementById('output').innerHTML = input
+
+
 })
-
